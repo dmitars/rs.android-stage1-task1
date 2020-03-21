fe0458a0b14bf0f473cd475ec8675fe9ce7b2cea
dmitars <dmitriy.tarasienko@gmail.com>
2020-03-21 23:01:56 +0300

    fix: change sum format to long

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,10 +5,13 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        var sum = bill.sum()
+          var sum = 0L
+        for(item in bill)
+            sum+=item
+
         sum -= bill[k]
         sum/=2
-        return if(sum!=b)
+        return if(sum!=b.toLong())
             (b-sum).toString()
         else
             "Bon Appetit"
